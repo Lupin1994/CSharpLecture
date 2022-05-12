967524ac8c05c81bf6919d26dadd87841b84874a
Alex <skorpion_94@list.ru>
2022-05-12 11:42:30 +0300

    Файлы с лекции

---
+++ example005_Hello_Alex/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write name :");
+string username = Console.ReadLine();
+if(username.ToLower() == "alex")
+{
+    Console.WriteLine("You the best man! Alex.");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
