b1a4b4e53b507164ec46939d7e916757c5177467
Alex <skorpion_94@list.ru>
2022-05-18 14:34:35 +0300

    Лекция 3 = задача 2 (Сортировка в массиве)

---
+++ 03.Example04_SortArray/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int[] arr = { 1, 7, 4, 6, 9, 2, 8, 4, 7 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
