1001b362a737c7b893ef33b94be33858dd9f04fd
Alex <skorpion_94@list.ru>
2022-05-18 23:10:09 +0300

    Лекция 3 = задача 2 (Сортировка в массиве Max)

---
+++ 03.Example05_SortArrayMax/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = { 1, 5, 9, 14, 3, 6, 8, 10, 2 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        int MaxPosition = i;
+        for (int j = i+1;j < array.Length; j++)
+        {
+            if (array[j]>array[MaxPosition]) MaxPosition = j;
+        }
+        int Temprorary = array[i];
+        array[i] = array[MaxPosition];
+        array[MaxPosition] = Temprorary;
+    }
+    Console.WriteLine(array.Length);
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
