995209d116ad700868969ac6e77201f15a919a06
Alex <skorpion_94@list.ru>
2022-06-09 21:27:12 +0300

    перебор слов

--- 07.Example_05/Program.cs
+++ 07.Example_05/Program.cs
@@ -2,22 +2,37 @@
 // В некотором машинном алфавите имеются 4 буквы "а" "и" "с" "в". 
 // Покажите все слова, состоящие из Т букв, которые можно построить из букв этого алфавита.
 
-char[] s = { 'а', 'и', 'с','в'};
-int count = s.Length;
+// char[] s = { 'а', 'и', 'с','в'};
+// int count = s.Length;
+// int n = 1;
+// for (int i = 0; i < count; i++)
+// {
+//     for (int j = 0; j < count; j++)
+//     {
+//         for (int k = 0; k < count; k++)
+//         {
+//             for (int l = 0; l < count; l++)
+//             {
+//                 for (int m = 0; m < count; m++)
+//                 {
+//                     Console.WriteLine($"{n++,-5}{s[i]}{s[j]}{s[k]}{s[l]}{s[m]}");
+//                 }
+//             }
+//         }  
+//     }
+// }
+
 int n = 1;
-for (int i = 0; i < count; i++)
+void FindWords(string alphabet, char[] word, int length = 0)
 {
-    for (int j = 0; j < count; j++)
+    if (length == word.Length)
     {
-        for (int k = 0; k < count; k++)
-        {
-            for (int l = 0; l < count; l++)
-            {
-                for (int m = 0; m < count; m++)
-                {
-                    Console.WriteLine($"{n++,-5}{s[i]}{s[j]}{s[k]}{s[l]}{s[m]}");
-                }
-            }
-        }  
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
     }
 }
+FindWords("аисв", new char[3]);
