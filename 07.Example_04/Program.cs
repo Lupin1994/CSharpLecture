fa342bcfe30479422cd1620008082c89fd6fd168
Alex <skorpion_94@list.ru>
2022-06-09 21:11:29 +0300

    Вычислить А в степени n

---
+++ 07.Example_04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Вычислить A в степени "n"
+int PowerFor(int A, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= A;
+    return result;
+}
+
+int PowerRec(int A, int n)
+{
+    //return n==0 ? : PowerRec(A, n-1) *A; тернарный оператор
+    if (n == 0) return 1;
+    else return PowerRec(A, n - 1) * A;
+}
+
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+}
+Console.WriteLine(PowerFor(2, 10)); // 1024
+Console.WriteLine(PowerRec(2, 10)); // 1024
+Console.WriteLine(PowerRecMath(2, 10)); // 1024
