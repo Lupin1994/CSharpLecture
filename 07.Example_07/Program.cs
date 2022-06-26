40131f1764ae02af48ebfc4ec3c89ba29fb34051
Alex <skorpion_94@list.ru>
2022-06-26 13:54:38 +0300

    Lecture 7

---
+++ 07.Example_07/Program.cs
@@ -0,0 +1,8 @@
+﻿string path = "/Users/tira/Desktop/Rubbish";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
